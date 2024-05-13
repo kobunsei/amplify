13ccba07984a946868252396e1deff0a7ea5a7de
kobunsei <kou@justsolution.co.jp>
2024-05-13 14:36:28 +0900

    プッシュ通知表示修正

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react'; // useRef をインポート
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -10,11 +10,11 @@ Amplify.configure(awsExports);
 function App({ signOut, user }) {
   const [isTokenFound, setTokenFound] = useState(false);
   const [notification, setNotification] = useState({ title: '', body: '' });
+  const notificationShown = useRef(new Set()); // 既に表示した通知の管理
 
   // プッシュ通知の許可をリクエスト
   useEffect(() => {
     requestForToken(setTokenFound);
-    // ブラウザの通知許可を求める
     if ('Notification' in window) {
       Notification.requestPermission();
     }
@@ -22,7 +22,7 @@ function App({ signOut, user }) {
 
   // フォアグラウンドで通知を受信
   useEffect(() => {
-    onMessageListener()
+    const unsubscribe = onMessageListener()
       .then((payload) => {
         setNotification({
           title: payload.notification.title,
@@ -33,16 +33,20 @@ function App({ signOut, user }) {
         console.log(payload);
       })
       .catch((err) => console.log('failed: ', err));
+
+    return () => unsubscribe; // コンポーネントアンマウント時にリスナーをクリーンアップ
   }, []);
 
   // ネイティブ通知を表示する関数
   const showNotification = (title, body) => {
-    if ('Notification' in window && window.Notification.permission === "granted") {
+    const notificationId = `${title}|${body}`; // 通知のユニークなIDを生成
+    if ('Notification' in window && window.Notification.permission === "granted" && !notificationShown.current.has(notificationId)) {
       console.log('Showing notification'); // 通知表示前にログを出力
       new Notification(title, {
         body: body,
         icon: 'logo192.png'
       });
+      notificationShown.current.add(notificationId); // 表示済みの通知に追加
     } else {
       console.log('Notification permission not granted or not supported'); // 条件分岐が失敗した場合のログ
     }
@@ -53,10 +57,7 @@ function App({ signOut, user }) {
       <header className="App-header">
         <h1>Welcome, {user.username}</h1>
         <button onClick={signOut}>Sign out</button>
-        {/* {isTokenFound ? <p>Notification permission enabled</p> : <p>Need notification permission</p>}
-        <h2>Notification:</h2>
-        <p>Title: {notification.title}</p>
-        <p>Body: {notification.body}</p> */}
+        {isTokenFound ? <p>Notification permission enabled</p> : <p>Need notification permission</p>}
       </header>
     </div>
   );
