3a8d0bbfc72d6acf0bef43119a184e69c467cccc
kobunsei <kou@justsolution.co.jp>
2024-05-13 11:42:24 +0900

    プッシュ通知表示修正

--- src/App.js
+++ src/App.js
@@ -38,7 +38,13 @@ function App({ signOut, user }) {
   // ネイティブ通知を表示する関数
   const showNotification = (title, body) => {
     if ('Notification' in window && window.Notification.permission === "granted") {
-      new Notification(title, { body, icon: "logo192.png" });
+      console.log('Showing notification'); // 通知表示前にログを出力
+      new Notification(title, {
+        body: body,
+        icon: 'logo192.png'
+      });
+    } else {
+      console.log('Notification permission not granted or not supported'); // 条件分岐が失敗した場合のログ
     }
   };
 
