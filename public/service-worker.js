e933a3523e643ff4ab9b0e2a4b7afadbe124ab90
kobunsei <kou@justsolution.co.jp>
2024-05-15 11:50:42 +0900

    重複送信修正5

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,39 +4,64 @@ const urlsToCache = [
   '/index.html'
 ];
 
+// キャッシュのインストール
 self.addEventListener('install', event => {
   console.log('[service-worker.js] Install event');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('[service-worker.js] Opened cache');
-        return Promise.all(
-          urlsToCache.map(url => {
-            return cache.add(url).catch(err => {
-              console.error(`[service-worker.js] Failed to cache ${url}:`, err);
-            });
-          })
-        );
+        return cache.addAll(urlsToCache);
       })
   );
 });
 
-
+// キャッシュのアクティベート
 self.addEventListener('activate', event => {
-  console.log('Service Worker activating.');
+  console.log('[service-worker.js] Activate event');
 });
 
+// リソースのフェッチ
 self.addEventListener('fetch', event => {
+  console.log('[service-worker.js] Fetch event for ', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return the response from the cached version
         if (response) {
+          console.log('[service-worker.js] Found in cache', event.request.url);
           return response;
         }
-
-        // Not in cache - fetch from the network
+        console.log('[service-worker.js] Network request for ', event.request.url);
         return fetch(event.request);
       })
   );
+});
+
+// Firebase メッセージングのインポート
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCznRcpO0AyCJIL0qwrMGbnHQXEx27X-YI',
+  authDomain: 'amplity-test.firebaseapp.com',
+  projectId: 'amplity-test',
+  storageBucket: "amplity-test.appspot.com",
+  messagingSenderId: '1038543419902',
+  appId: '1:1038543419902:web:dcea33dbe03e1aa85068af'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
+
+  console.log('[firebase-messaging-sw.js] Showing notification');
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
