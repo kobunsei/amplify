36af5b26457640bae9f93c285697e570cbb424da
kobunsei <kou@justsolution.co.jp>
2024-05-16 16:02:00 +0900

    重複送信修正11

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
 // importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyCznRcpO0AyCJIL0qwrMGbnHQXEx27X-YI',
-    authDomain: 'amplity-test.firebaseapp.com',
-    projectId: 'amplity-test',
-    storageBucket: "amplity-test.appspot.com",
-    messagingSenderId: '1038543419902',
-    appId: '1:1038543419902:web:dcea33dbe03e1aa85068af'
-};
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyCznRcpO0AyCJIL0qwrMGbnHQXEx27X-YI',
+//     authDomain: 'amplity-test.firebaseapp.com',
+//     projectId: 'amplity-test',
+//     storageBucket: "amplity-test.appspot.com",
+//     messagingSenderId: '1038543419902',
+//     appId: '1:1038543419902:web:dcea33dbe03e1aa85068af'
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
